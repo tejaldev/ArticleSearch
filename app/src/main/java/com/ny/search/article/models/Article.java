d25dc5b29bfaab1ab537a46aa008d6a07c90d04f
Tejal Par <parulekar.t@gmail.com>
2017-09-23 01:08:29 -0700

    add data models

---
+++ app/src/main/java/com/ny/search/article/models/Article.java
@@ -0,0 +1,77 @@
+package com.ny.search.article.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Article data model
+ *
+ * @author tejalpar
+ */
+public class Article implements Parcelable {
+    public static final String BASE_IMAGE_PATH = "http://www.nytimes.com/";
+    public static final String PARCEL_KEY = "ARTICLE_DATA";
+
+    public String articleId;
+    public String snippet;
+    public String headline;
+    public String newDesk;
+    public String webUrl;
+    public String wideUrl;
+    public String xlargeUrl;
+    public String thumbnailUrl;
+
+    public Article() {}
+
+    public Article(Parcel source) {
+        // read values from parcel
+        readFromParcel(source);
+    }
+
+    public static String getAbsoluteMoviePath(String relativePath) {
+        return BASE_IMAGE_PATH + relativePath;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(articleId);
+        dest.writeString(snippet);
+        dest.writeString(newDesk);
+        dest.writeString(headline);
+        dest.writeString(webUrl);
+        dest.writeString(wideUrl);
+        dest.writeString(xlargeUrl);
+        dest.writeString(thumbnailUrl);
+    }
+
+    private void readFromParcel(Parcel source) {
+        // read in same order as written
+        articleId = source.readString();
+        snippet = source.readString();
+        newDesk = source.readString();
+        headline = source.readString();
+        webUrl = source.readString();
+        wideUrl = source.readString();
+        xlargeUrl = source.readString();
+        thumbnailUrl = source.readString();
+    }
+
+    public static final Creator<Article> CREATOR
+            = new Creator<Article>() {
+
+        @Override
+        public Article createFromParcel(Parcel source) {
+            return new Article(source);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[0];
+        }
+    };
+}
---
+++ app/src/main/java/com/ny/search/article/models/Filter.java
@@ -0,0 +1,52 @@
+package com.ny.search.article.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * @author tejalpar
+ */
+public class Filter {
+    public static final String ARTS_KEY = "Arts";
+    public static final String FASHION_KEY = "Fashion";
+    public static final String SPORTS_KEY = "Sports";
+
+    private long dateInMillis;
+    private String sortOrder;
+    private HashMap<String, Boolean> newsDeskMap;
+
+    public long getDateInMillis() {
+        return dateInMillis;
+    }
+
+    public void setDateInMillis(long dateInMillis) {
+        this.dateInMillis = dateInMillis;
+    }
+
+    public String getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(String sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public HashMap<String, Boolean> getNewsDeskMap() {
+        return newsDeskMap;
+    }
+
+    public void setNewsDeskMap(HashMap<String, Boolean> newsDeskMap) {
+        this.newsDeskMap = newsDeskMap;
+    }
+
+    public boolean isFilterSet() {
+        return (dateInMillis != 0);
+    }
+
+    public String getFormattedDate() {
+        Date date = new Date(dateInMillis);;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        return simpleDateFormat.format(date);
+    }
+}
